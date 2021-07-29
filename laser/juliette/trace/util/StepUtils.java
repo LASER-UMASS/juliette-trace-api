a8188a8d1bb9826f16e0e3e6aad74c69980a732a
Heather M Conboy <hconboy@cs.umass.edu>
2021-07-29 09:47:37 -0400

    Modified the StepUtils class to add the isX methods to support introspection.

--- laser/juliette/trace/util/StepUtils.java
+++ laser/juliette/trace/util/StepUtils.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import laser.juliette.trace.Event;
 import laser.juliette.trace.EventFilter;
+import laser.juliette.trace.ReactiveController;
 import laser.juliette.trace.StateChangeEvent;
 import laser.juliette.trace.Trace;
 import laser.juliette.trace.filter.AllEventFilter;
@@ -77,9 +78,132 @@ public class StepUtils
 			{
 				children.add(currentEvent);
 			}
+			else if ((ancestorEvent instanceof StateChangeEvent) &&
+					 (currentEvent instanceof StateChangeEvent) &&
+					 StepUtils.isParentStep((StateChangeEvent)ancestorEvent, (StateChangeEvent)currentEvent) &&
+					 filter.accept(currentEvent)) 
+			{
+				children.add(currentEvent);
+			}
 		} // end for eventsItr
 		
 		return children;
+	}
+	
+	/**
+	 * Returns the unique step ID for the given agenda item.
+	 * 
+	 * @param event The event corresponding to the agenda item
+	 * @return The unique step ID for that agenda item
+	 */
+	public static String getUniqueStepID(StateChangeEvent event) {
+		return event.getAnnotation(StateChangeEvent.UNIQUE_STEP_ID_ANNOTATION_NAME).getValue();
+	}
+	
+	/**
+	 * Returns whether or not the given agenda item corresponds to a leaf step.
+	 * 
+	 * @param event The event corresponding to the agenda item
+	 * @return True if that agenda item corresponds to a leaf step and false otherwise
+	 */
+	public static boolean isLeaf(StateChangeEvent event) {
+		return (event.getSequencingKind() == StateChangeEvent.SequencingKind.leaf);
+	}
+
+	/**
+	 * Returns whether or not the given agenda item corresponds to a non-leaf step.
+	 * 
+	 * @param event The event corresponding to the agenda item
+	 * @return True if that agenda item corresponds to a non-leaf step and false otherwise
+	 */
+	public static boolean isNonLeaf(StateChangeEvent event) {
+		return (! StepUtils.isLeaf(event));
+	}
+
+	/**
+	 * Returns whether or not the given agenda item corresponds to a sequential non-leaf step.
+	 * 
+	 * @param event The event corresponding to the agenda item
+	 * @return True if that agenda item corresponds to a sequential non-leaf step and false otherwise
+	 */
+	public static boolean isSequential(StateChangeEvent event) {
+		StateChangeEvent.SequencingKind seqKind = event.getSequencingKind();
+		
+		return ((seqKind == StateChangeEvent.SequencingKind.nonleaf_seq) ||
+				(seqKind == StateChangeEvent.SequencingKind.nonleaf_try));
+	}
+	
+	/**
+	 * Returns whether or not the given agenda item corresponds to a concurrent non-leaf step.
+	 * 
+	 * @param event The event corresponding to the agenda item
+	 * @return True if that agenda item corresponds to a concurrent non-leaf step and false otherwise
+	 */
+	public static boolean isConcurrent(StateChangeEvent event) {
+		StateChangeEvent.SequencingKind seqKind = event.getSequencingKind();
+		
+		return ((seqKind == StateChangeEvent.SequencingKind.nonleaf_par) ||
+				(seqKind == StateChangeEvent.SequencingKind.nonleaf_cho));		
+	}
+	
+	/**
+	 * Returns whether or not first agenda item is the parent of the second agenda item.
+	 * 
+	 * @param event1 The event corresponding to the first agenda item
+	 * @param event2 The event corresponding to the second agenda item
+	 * @return True if the first agenda item is the parent of the second agenda item
+	 */
+	public static boolean isParentStep(StateChangeEvent event1, StateChangeEvent event2) {
+		StateChangeEvent parentEvent = (StateChangeEvent)event2.getAncestor();
+		
+		// The parent must be non-null.
+		if ((event1 == null) || (parentEvent == null)) {
+			return false;
+		}
+		
+		// The Little-JIL execution semantics state that the parent has started 
+		// and the child has posted.
+		if (event1 == parentEvent) {
+			return true;
+		}
+		
+		// The parent is at one of its states and the child is at one of its states.
+		String event1ID = StepUtils.getUniqueStepID(event1);
+		String parentEventID = StepUtils.getUniqueStepID(parentEvent);
+		if (event1ID.equals(parentEventID)) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Returns whether or not the two agenda items are siblings.
+	 * 
+	 * @param event1 The event corresponding to the first agenda item
+	 * @param event1 The event corresponding to the second agenda item
+	 * @return True if the two agenda items are siblings and false otherwise
+	 */
+	public static boolean isSiblingStep(StateChangeEvent event1, StateChangeEvent event2) {
+		StateChangeEvent parentEvent1 = (StateChangeEvent)event1.getAncestor();
+		StateChangeEvent parentEvent2 = (StateChangeEvent)event2.getAncestor();
+		
+		// The parent must be non-null.
+		if ((parentEvent1 == null) || (parentEvent2 == null)) {
+			return false;
+		}
+		
+		String parentEventID1 = StepUtils.getUniqueStepID(parentEvent1);
+		String parentEventID2 = StepUtils.getUniqueStepID(parentEvent2);
+		String stepID1 = StepUtils.getUniqueStepID(event1);
+		String stepID2 = StepUtils.getUniqueStepID(event2);
+		if (parentEventID1.equals(parentEventID2) && 
+			(! stepID1.equals(stepID2))) 
+		{
+			return true;
+		}
+		
+		return false;
 	}
 
 	/**
